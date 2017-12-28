99ee7cab69c9e954ebee87d64765b5479b6b959b
Hard Problems <Nishad Mandlik>
2017-12-28 15:30:21 +0530

    TTS Engine Changed to E-Speak

--- STTTS.py
+++ STTTS.py
@@ -8,26 +8,28 @@ Created on Thu Dec 28 10:27:47 2017
 
 print ("Please Wait")
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 import os
 from time import sleep
-import pyttsx
 
-TTS = pyttsx.init()
 pwd = os.path.dirname(__file__)
 
 chrOpts = webdriver.ChromeOptions()
 chrOpts.add_argument("--use-fake-ui-for-media-stream")
 
-fireProf = webdriver.FirefoxProfile(pwd + "/Firefox Profile")
+os.system('espeak "Start speaking after the beep"')
 
-#driver = webdriver.Firefox(firefox_profile = fireProf, executable_path = pwd + '/geckodriver')
+#fireProf = webdriver.FirefoxProfile(pwd + "/Firefox Profile")
+
 driver = webdriver.Chrome(executable_path = pwd + '/chromedriver', chrome_options = chrOpts)
+#driver = webdriver.Firefox(firefox_profile = fireProf, executable_path = pwd + '/geckodriver')
 
 driver.get("https://mandliksg.000webhostapp.com")
 
 while (driver.title != "Recording") :
     sleep(1)
+    
+os.system("aplay " + pwd + "/Beep.wav")
 
 print ("Recording")
 while (driver.title != "Done") :
@@ -36,11 +38,10 @@ while (driver.title != "Done") :
 print ("Done")
   
 STT = driver.find_element_by_id("confm").text
+print (STT)
 driver.close()
 
-TTS.say(STT)
-TTS.runAndWait()
-
+os.system('espeak "' + STT + '"')
 
 
 
