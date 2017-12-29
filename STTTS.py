049060110b90c58ac48fa0bb75b74824c48ce9b9
Hard Problems <Nishad Mandlik>
2017-12-29 16:23:50 +0530

    Selenium Removed. Compatible With Raspberry Pi

--- STTTS.py
+++ STTTS.py
@@ -5,45 +5,63 @@ Created on Thu Dec 28 10:27:47 2017
 
 @author: Nishad Mandlik
 """
-
-print ("Please Wait")
-from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from chromote import Chromote
 import os
 from time import sleep
+from threading import Thread
 
-pwd = os.path.dirname(os.path.abspath(__file__))
+def BrowSt () :
+    os.system("chromium-browser -remote-debugging-port=9222")
+    
+def WelMes () :
+    os.system('espeak "Please start your command after the beep"')
+ 
 
-chrOpts = webdriver.ChromeOptions()
-chrOpts.add_argument("--use-fake-ui-for-media-stream")
+print ("Please Wait . . .")
+WelcTh = Thread(target=WelMes)
+WelcTh.start()
 
 os.system("jack_control start")
 os.system("sudo amixer cset numid=3 1")
+pwd = os.path.dirname(os.path.abspath(__file__))
 
-os.system('espeak "Please start speaking after the beep"')
+BrowTh = Thread(target=BrowSt)
+BrowTh.start()
+launchAttempts = 0
+while (launchAttempts<20):
+    try:        
+        sleep(1)
+        launchAttempts = launchAttempts + 1
+        krom = Chromote()
+    except :
+        continue
+    break
 
-fireProf = webdriver.FirefoxProfile(pwd + "/Firefox Profile")
-
-driver = webdriver.Chrome(executable_path = pwd + "/chromedriver", chrome_options = chrOpts)
-#driver = webdriver.Firefox(firefox_profile = fireProf, firefox_binary = '/usr/bin/firefox', executable_path = pwd + '/geckodriver')
-
-driver.get("https://mandliksg.000webhostapp.com")
-
-while (driver.title != "Recording") :
+if (launchAttempts==20):
+    WelcTh.join()
+    ErrMes = "Maximum Attempts Reached. Your Browser Has Either Not Started Or Has Remote Debugging Disabled"
+    print (ErrMes)
+    os.system('espeak "' + ErrMes + '"')
+    
+TabSTT = krom.tabs[0]
+TabSTT.set_url("https://mandliksg.000webhostapp.com")
+WelcTh.join()
+while True:
     sleep(1)
+    PagSour=TabSTT.html
+    if (PagSour[PagSour.find("<title>")+7:PagSour.find("</title>")]=="Recording"):
+        break
     
 os.system("aplay " + pwd + "/Beep.wav")
 
-print ("Recording")
-while (driver.title != "Done") :
+while True:
     sleep(1)
-    
-print ("Done")
-  
-STT = driver.find_element_by_id("confm").text
-print (STT)
-driver.close()
+    PagSour=TabSTT.html
+    if (PagSour[PagSour.find("<title>")+7:PagSour.find("</title>")]=="Done"):
+        break
 
+PagSour=TabSTT.html
+STT = PagSour[PagSour.find('<span id="confm">')+17 : PagSour.find("</span>")]
+krom.close_tab(TabSTT)
+print(STT)
 os.system('espeak "' + STT + '"')
-
-
